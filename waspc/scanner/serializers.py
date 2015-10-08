742ccf4e05017ea301c707540f8ef12d45075486
em <mailto@zensecurity.su>
2015-10-08 19:28:34 +0300

    Serializers updated.

--- waspc/scanner/serializers.py
+++ waspc/scanner/serializers.py
@@ -1,6 +1,7 @@
 from .models import ScanReport
 from django.conf import settings
 from rest_framework.serializers import (DateTimeField,
+                                        ModelField,
                                         ModelSerializer,
                                         URLField,
                                         UUIDField)
@@ -9,9 +10,9 @@ from rest_framework.serializers import (DateTimeField,
 class ScannerSerializer(ModelSerializer):
     id = UUIDField(read_only=True)
     target_url = URLField(initial=settings.WASPC['scanner']['target_url'])
+    report = ModelField(model_field=ScanReport()._meta.get_field('result'), read_only=True)
     result_url = URLField(read_only=True)
     modified = DateTimeField(read_only=True)
 
     class Meta:
         model = ScanReport
-        fields = ('id', 'target_url', 'result_url', 'modified')
