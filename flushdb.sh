00fbd109668ceb5791c20d0f64268c8d3f9fee96
em <mailto@zensecurity.su>
2015-11-03 19:32:24 +0300

    flushdb.sh updated.

--- flushdb.sh
+++ flushdb.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+OS=`uname`
+
+if [[ $OS == Darwin ]]; then
+    shopt -s expand_aliases
+    alias psql="psql95"
+fi
+
+echo -e "\n[ Creating template for unicode database ]"
 psql -c "UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1'"
 psql -c "DROP DATABASE template1"
 psql -c "CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE'"
 psql -c "UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1'"
 
+echo -e "\n[ Creating waspc database, user and grant rights]"
 psql -c "DROP DATABASE waspc"
 psql -c "DROP USER waspc"
 psql -c "CREATE DATABASE waspc encoding=UTF8"
 psql -c "CREATE USER waspc WITH PASSWORD 'waspc'"
 psql -c "GRANT ALL PRIVILEGES ON DATABASE waspc to waspc"
+
+echo ""
