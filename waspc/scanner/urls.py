35896b3e428b1a9117db635b5096cd2021e9a1b4
em <mailto@zensecurity.su>
2015-11-13 16:31:01 +0300

    Small fixes added.

--- waspc/scanner/urls.py
+++ waspc/scanner/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^$', ScannerTemplateView.as_view()),
+    url(r'^$', ScannerTemplateView.as_view(), name='root'),
     url(r'^report/(?P<pk>[0-9a-f-]+)/$', ReportTemplateView.as_view(), name='report')
 ]
