fe58f2b3a127ca5ede1b2fef9e84695c58b9679e
em <mailto@zensecurity.su>
2015-12-14 15:27:23 +0300

    Fixed removing monitor with periodic task simultaneously.

--- waspc/monitoring/models.py
+++ waspc/monitoring/models.py
@@ -1,16 +1,27 @@
 from django.db.models import (DateTimeField,
                               Model,
                               OneToOneField,
+                              QuerySet,
                               UUIDField)
 from djcelery.models import IntervalSchedule, PeriodicTask
 from ..scanner.models import Report
 from uuid import uuid4
 
 
+class MonitorQuerySet(QuerySet):
+    def delete(self, *args, **kwargs):
+        for monitor in self:
+            monitor.periodic_task.delete()
+            monitor.delete()
+        super(MonitorQuerySet, self).delete(*args, **kwargs)
+
+
 class Monitor(Model):
     """
     Monitor information
     """
+    objects = MonitorQuerySet.as_manager()
+
     id = UUIDField(primary_key=True, default=uuid4)
     report = OneToOneField(Report, blank=True, null=True)
     periodic_task = OneToOneField(PeriodicTask)
