076b2b4e236d801ee68238a66f68c065ce50a57d
Mikhail Yushkovskiy <mailto@zensecurity.su>
2016-08-01 12:38:04 +0300

    Update flushdb.sh

--- flushdb.sh
+++ flushdb.sh
@@ -9,12 +9,12 @@ fi
 
 echo -e "\n[ Creating template for unicode databases ]"
 psql -c "UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1'"
-psql -c "DROP DATABASE template1"
+psql -c "DROP DATABASE IF EXISTS template1"
 psql -c "CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE'"
 psql -c "UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1'"
 
 echo -e "\n[ Creating waspc database, user and grant rights]"
-psql -c "DROP DATABASE waspc"
+psql -c "DROP DATABASE IF EXISTS waspc"
 psql -c "DROP USER waspc"
 psql -c "CREATE DATABASE waspc encoding='UTF8'"
 psql -c "CREATE USER waspc WITH PASSWORD 'waspc'"
